9dc9ba77328f34af5272ceb0ae85c10543929695
liuhang <lhang1797618226@126.com>
2019-08-02 22:45:34 +0800

    第一次提交

---
+++ src/main/java/life/lhang/itcommunity/ItcommunityApplication.java
@@ -0,0 +1,13 @@
+package life.lhang.itcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItcommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/lhang/itcommunity/ItcommunityApplicationTests.java
@@ -0,0 +1,16 @@
+package life.lhang.itcommunity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItcommunityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
