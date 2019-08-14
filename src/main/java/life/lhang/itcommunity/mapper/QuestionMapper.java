7b30a2591d066a8db8e83c62acb34fa8d32d7293
liuhang <lhang1797618226@126.com>
2019-08-15 01:10:09 +0800

    修复发布页面和代码

--- src/main/java/life/lhang/itcommunity/mapper/QuestionMapper.java
+++ src/main/java/life/lhang/itcommunity/mapper/QuestionMapper.java
@@ -7,13 +7,13 @@ import org.apache.ibatis.annotations.Update;
 
 public interface QuestionMapper{
 
-    @Insert("insert into question (id, title, gmtCreate, gmtModified, creator, commentCount, viewCount, likeCount, tag, description) values (#{id}, #{title}, #{gmtCreate}, #{gmtModified}, #{creator}, #{commentCount}, #{viewCount}, #{likeCount}, #{tag}, #{description})")
+    @Insert("insert into question (id, title, gmt_create, gmt_modified, creator, comment_count, view_count, like_count, tag, description) values (#{id}, #{title}, #{gmtCreate}, #{gmtModified}, #{creator}, #{commentCount}, #{viewCount}, #{likeCount}, #{tag}, #{description})")
     void insert(Question question);
 
     @Select("select * from question where id = #{id}")
     Question selectByPrimaryKey(Long id);
 
-    @Update("update question set gmtModified=#{gmtModified}, title=#{title}, description=#{description},tag=#{tag} where id =#{id}")
+    @Update("update question set gmt_modified=#{gmtModified}, title=#{title}, description=#{description},tag=#{tag} where id =#{id}")
     int update(Question updateQuestion);
 
 }
